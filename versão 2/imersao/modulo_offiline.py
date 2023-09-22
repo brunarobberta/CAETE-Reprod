b1e5ded028cb203633cf637307c283bafa0d0c76
brunarobberta <brunarobertasoica.com>
2023-09-22 11:08:49 -0300

    Init

--- versão 2/imersao/modulo_offiline.py
+++ versão 2/imersao/modulo_offiline.py
@@ -16,25 +16,32 @@ pls = []
 
 # Geração das propriedades das espécies
 for i in range(n_pls):
-    wood_density = round(random.uniform(0.5, 0.9), 1)  # Quilogramas por metro cúbico
-    wood_density = wood_density / 1000  # Converte para grama por cm3
+    #wood_density = round(random.uniform(0.5, 0.9), 1)  # Quilogramas por metro cúbico
+    wood_density = round(random.uniform(0.34, 1.06), 1)  # g/cm3
+    wood_density = wood_density * 1000000  # Converte para grama por m3
+    wood_density = wood_density / 2 #converte em gC/m3
+    wood_density = wood_density / 1000 #converte em kg/m3
     npp_disponivel = round(random.uniform(0, 1.3), 1)  # quilogramas de carbono por m2
     npp_disponivel = npp_disponivel * 1000  # converte para gramas de carbono por m2
     npp_cstem = npp_disponivel * 0.35
-    cstem = npp_cstem
-    npp_disponivel -= npp_cstem
+    npp_rep = (npp_disponivel - npp_cstem) * 0.10  # 10% do que sobra após a alocação para c_stem
+    cstem = npp_cstem 
     #cstem = round(random.uniform(5, 20), 1)  # kgC
     #cstem = cstem * 1000  # Converte em g de carbono
-    diam = round((4 * cstem) / (wood_density * pi * 40) ** 0.5 + 0.5, 1)  # Diâmetro do caule em cm
+    #diam = round((4 * cstem) / (wood_density * pi * 40) ** 0.5 + 0.5, 1)  # Diâmetro do caule em cm
+    diam = round(4 * cstem * 1.0e3) / (wood_density * 1.0e6 * pi * Kallom2)**(1 / (2 + Kallom3))
+
     height = round(Kallom2 * diam ** Kallom3, 1)  # Altura em cm
 
     # Cálculo da massa da semente
-    mass_seed = (height / 10) ** (0.08) ** (1 / 0.43)
+    #mass_seed = (height / 10) ** 0.08 ** (1 / 0.43)
+    mass_seed = (10 ** (0.08 * (height / 10))) ** (1 / 0.43)
+
 
     pls.append({
         'seed_bank': 0,
-        'npp_disponivel': 0,
-        'npp_rep': 0,
+        'npp_disponivel': npp_disponivel,
+        'npp_rep': npp_rep,
         'mass_seed': mass_seed,
         'wood_density': wood_density,
         'cstem': cstem,
@@ -45,10 +52,11 @@ for i in range(n_pls):
 
 def reproduction(temperature, precipitation, seed_bank, mass_seed, npp_rep):
     seed_production = 0  # Valor padrão para seed_production
-    if 23 <= temperature <= 30 and 50 <= precipitation <= 200:
+    if 23 <= temperature <= 30 and 50 <= precipitation <= 200 and npp_rep <= 0:
         seed_production = npp_rep / mass_seed
         seed_bank += int(seed_production)  # Convertendo para inteiro
     return seed_bank, seed_production, npp_rep
+
 
 def germination(seed_bank, temperature):
     if seed_bank > 0 and 23 <= temperature <= 30:
@@ -56,6 +64,7 @@ def germination(seed_bank, temperature):
         seed_bank -= germinated_seeds  # Subtrair as sementes germinadas do banco
         return germinated_seeds
     return 0
+####inserie as q nao germinam vao pra liteira
 
 # Lista para armazenar os dados para o gráfico
 data_points = []
@@ -80,30 +89,30 @@ while years < 10:
             #npp_disponivel = npp_disponivel * 1000  # converte para gramas de carbono por m2
             #npp_cstem = npp_disponivel * 0.35
             #npp_disponivel -= npp_cstem
-            npp_rep = npp_disponivel * 0.1
+            #npp_rep = npp_disponivel * 0.1
 
             #germination_temperature = round(random.uniform(23, 30), 1)  # Graus Celsius
 
-            if npp_rep <= 0:
-                continue
-            else:
-                plss['npp_rep'] = npp_rep
+        #    if npp_rep <= 0:
+        #        continue
+        #    else:
+        #        plss['npp_rep'] = npp_rep
 
             print(f"Espécie {pls_id + 1} - Ano {years + 1}, Dia {day + 1}")
             print("temperatura:", temperature, "°C")
             print("precipitação:", precipitation, "mm")
-            print("densidade da madeira:", plss['wood_density'], "g/cm³")
+            print("densidade da madeira:", plss['wood_density'], "kg/m³")
             print("npp_cstem", plss['npp_cstem'], "gC")
             print("cstem:", plss['cstem'], "gC")
             print("diam:", plss['diam'], "cm")
             print("altura:", plss['height'], "cm")
-            print("massa da semente:", plss['mass_seed'])
-            print("npp_rep", npp_rep)
-            print("npp_disponivel", npp_disponivel)
+            print("massa da semente:", plss['mass_seed'], "g")
+            print("npp_rep", plss['npp_rep'])
+            print("npp_disponivel", plss ['npp_disponivel'])
 
             print("\nReprodução:")
             print(f"Banco de sementes antes da reprodução (pls {pls_id + 1}): {plss['seed_bank']}")
-            plss['seed_bank'], seed_production, plss['npp_rep'] = reproduction(temperature, precipitation, plss['seed_bank'], plss['mass_seed'], npp_rep)
+            plss['seed_bank'], seed_production, plss['npp_rep'] = reproduction(temperature, precipitation, plss['seed_bank'], plss['mass_seed'], plss['npp_rep'])
             print(f"Sementes produzidas (pls {pls_id + 1}): {int(seed_production)}")
             print(f"Banco de sementes após a reprodução (pls {pls_id + 1}): {plss['seed_bank']}")
 
@@ -137,8 +146,8 @@ plt.figure(figsize=(10, 6))
 plt.scatter(wood_densities, mass_seeds, color='blue', marker='o', label='Dados')
 plt.plot(wood_densities, poly(wood_densities), color='red', label='Regressão Linear')
 plt.title('Trade-off entre Massa da Semente e Densidade da Madeira')
-plt.xlabel('Densidade da Madeira (g/cm³)')
-plt.ylabel('Massa da Semente')
+plt.xlabel('Densidade da Madeira (kg/m³)')
+plt.ylabel('Massa da Semente (g)')
 plt.legend()
 plt.grid(True)
 plt.show()
